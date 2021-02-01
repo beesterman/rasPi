29d91e56d7fb647fc94e4ad90abe402a79065969
beesterman <beestermen@yahoo.com>
2021-01-31 22:49:17 -0600

    stuff

--- ledStuff/ledFlash.py
+++ ledStuff/ledFlash.py
@@ -1,10 +1,25 @@
 from gpiozero import LED
+from gpiozero import Button
 from time import sleep
 
 red = LED(17)
+button = button(2)
+
+
+
 
 while True:
-    red.on()
-    sleep(1)
-    red.off()
-    sleep(1)
+    if button.is_pressed:
+        red.off()
+    else:
+        red.on()
+
+
+
+
+
+
+    # red.on()
+    # sleep(1)
+    # red.off()
+    # sleep(1)
